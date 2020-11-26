07ac1f3abca25c6f0c9945fb926e39a6f54dc8c4
AlexStrokov <62788259+AlexStrokov@users.noreply.github.com>
2020-11-26 16:45:03 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,50 @@
-provider "google" {
-  project     = "testuvannya01"
-  region      = "us-west1"
-  zone        = "us-west1-b"
-  user_project_override = true
+resource "tls_private_key" "ssh-key" {
+  algorithm   = "RSA"
 }
-resource "google_compute_instance" "my_instance" {
-  name = "terraform${count.index}"
-  count = 1
-  machine_type = "e2-small"
-  allow_stopping_for_update = true
+
+provider "google" {
+  credentials = var.credentials
+  project = var.project 
+  region  = var.region
+}
+
+resource "google_compute_instance" "terraform" {
+  name         = "monitoring-system"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+  tags         = ["http-server", "https-server"]
+
   boot_disk {
     initialize_params {
-      image = "ubuntu-1804-bionic-v20201014"
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
     }
   }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+
+    }
+  }
+  
+  metadata = {
+    ssh-keys = "${var.user}:${tls_private_key.ssh-key.public_key_openssh}"
+  }
+  
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      port        = 22
+      user        = var.user
+      timeout     = "300s"
+      private_key = tls_private_key.ssh-key.private_key_pem
+      host = google_compute_instance.terraform.network_interface.0.access_config.0.nat_ip
+    }
+
+    inline = [
+      "touch temp.txt"
+    ]
+  }
+  
 }
