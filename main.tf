bd31868f7b2eafbb393ce97e14b6aab6471987ab
AlexStrokov <62788259+AlexStrokov@users.noreply.github.com>
2020-11-26 16:40:02 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ provider "google" {
   project     = "testuvannya01"
   region      = "us-west1"
   zone        = "us-west1-b"
-  credentials = "$GOOGLE_CREDENTIALS"
   user_project_override = true
 }
 resource "google_compute_instance" "my_instance" {
